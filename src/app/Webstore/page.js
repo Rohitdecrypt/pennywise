948b49a6ce080b13a532579f1347bf92f08631b5
Rohitdecrypt <rohit.decrypt@gmail.com>
2023-09-12 18:34:04 +0530

    StoreCredit in progress

--- src/app/Webstore/page.js
+++ src/app/Webstore/page.js
@@ -1,10 +1,11 @@
 import React from "react";
 import search from "../assets/search.png";
 import Image from "next/image";
+import {PiPencil} from "react-icons/pi"
 
 export default function () {
   return (
-    <div className="container mx-auto border">
+    <div className="container mx-auto ">
       <div className="flex mt-5 w-full items-center">
         <div className="text-center text-[#000000] font-bold">
           WEBSTORE FUNCTIONS
@@ -13,7 +14,7 @@ export default function () {
       </div>
       <div className="relative">
         <input
-          className="border border-custom-border-green rounded-full w-[40%] py-2 mt-5 px-16 text-custom leading-tight focus:outline-none focus:shadow-outline text-md placeholder-[#369EA4]"
+          className="border border-custom-border-green rounded-full w-[40%] py-2 mt-5 px-12 text-custom leading-tight focus:outline-none focus:shadow-outline text-md placeholder-[#369EA4]"
           id="username"
           type="text"
           placeholder="Search"
@@ -21,8 +22,182 @@ export default function () {
         <Image
           src={search}
           alt="Profile Image"
-          className="w-auto absolute bottom-3 left-4"
+          className=" absolute w-5 bottom-2 left-4"
         />
+      </div>
+      <div className="mt-10 border border-black rounded-2xl py-3 px-2">
+        <div className="  px-10 overflow-x-auto overflow-y-auto max-h-[700px] ">
+          <table className=" min-w-full text-[#000000] ">
+            <thead className="sticky top-0 bg-[#fff] py-2">
+              <tr className="border-b border-[#BFBFBF]  text-sm">
+                <th className="w-[10%]  text-start py-3 font-normal">
+                  Stock No.
+                </th>
+                <th className="w-[30%]  text-start py-3 font-normal">
+                  Item Description
+                </th>
+                <th className="text-start py-3 font-normal">
+                  Webstore Item URL
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="border-b border-[#BFBFBF] text-sm">
+                <td className="py-3 font-light">000000</td>
+                <td className="py-3 font-light">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit...
+                </td>
+                <td className="py-3 font-light">
+                  https://www.examplewebsitename.com.au/Lorem-Ipsum-Dolor-Sit-Amet-Consectetur-Adipiscing-Elit.
+                </td>
+              </tr>
+              <tr className="border-b border-[#BFBFBF] text-sm">
+                <td className="py-3 font-light">000000</td>
+                <td className="py-3 font-light">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit...
+                </td>
+                <td className="py-3 font-light">
+                  https://www.examplewebsitename.com.au/Lorem-Ipsum-Dolor-Sit-Amet-Consectetur-Adipiscing-Elit.
+                </td>
+              </tr>
+              <tr className="border-b border-[#BFBFBF] text-sm">
+                <td className="py-3 font-light">000000</td>
+                <td className="py-3 font-light">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit...
+                </td>
+                <td className="py-3 font-light">
+                  https://www.examplewebsitename.com.au/Lorem-Ipsum-Dolor-Sit-Amet-Consectetur-Adipiscing-Elit.
+                </td>
+              </tr>
+              <tr className="border-b border-[#BFBFBF] text-sm">
+                <td className="py-3 font-light">000000</td>
+                <td className="py-3 font-light">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit...
+                </td>
+                <td className="py-3 font-light">
+                  https://www.examplewebsitename.com.au/Lorem-Ipsum-Dolor-Sit-Amet-Consectetur-Adipiscing-Elit.
+                </td>
+              </tr>
+              <tr className="border-b border-[#BFBFBF] text-sm">
+                <td className="py-3 font-light">000000</td>
+                <td className="py-3 font-light">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit...
+                </td>
+                <td className="py-3 font-light">
+                  https://www.examplewebsitename.com.au/Lorem-Ipsum-Dolor-Sit-Amet-Consectetur-Adipiscing-Elit.
+                </td>
+              </tr>
+              <tr className="border-b border-[#BFBFBF] text-sm">
+                <td className="py-3 font-light">000000</td>
+                <td className="py-3 font-light">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit...
+                </td>
+                <td className="py-3 font-light">
+                  https://www.examplewebsitename.com.au/Lorem-Ipsum-Dolor-Sit-Amet-Consectetur-Adipiscing-Elit.
+                </td>
+              </tr>
+              <tr className="border-b border-[#BFBFBF] text-sm">
+                <td className="py-3 font-light">000000</td>
+                <td className="py-3 font-light">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit...
+                </td>
+                <td className="py-3 font-light">
+                  https://www.examplewebsitename.com.au/Lorem-Ipsum-Dolor-Sit-Amet-Consectetur-Adipiscing-Elit.
+                </td>
+              </tr>
+              <tr className="border-b border-[#BFBFBF] text-sm">
+                <td className="py-3 font-light">000000</td>
+                <td className="py-3 font-light">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit...
+                </td>
+                <td className="py-3 font-light">
+                  https://www.examplewebsitename.com.au/Lorem-Ipsum-Dolor-Sit-Amet-Consectetur-Adipiscing-Elit.
+                </td>
+              </tr>
+              <tr className="border-b border-[#BFBFBF] text-sm">
+                <td className="py-3 font-light">000000</td>
+                <td className="py-3 font-light">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit...
+                </td>
+                <td className="py-3 font-light">
+                  https://www.examplewebsitename.com.au/Lorem-Ipsum-Dolor-Sit-Amet-Consectetur-Adipiscing-Elit.
+                </td>
+              </tr>
+              <tr className="border-b border-[#BFBFBF] text-sm">
+                <td className="py-3 font-light">000000</td>
+                <td className="py-3 font-light">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit...
+                </td>
+                <td className="py-3 font-light">
+                  https://www.examplewebsitename.com.au/Lorem-Ipsum-Dolor-Sit-Amet-Consectetur-Adipiscing-Elit.
+                </td>
+              </tr>
+              <tr className="border-b border-[#BFBFBF] text-sm">
+                <td className="py-3 font-light">000000</td>
+                <td className="py-3 font-light">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit...
+                </td>
+                <td className="py-3 font-light">
+                  https://www.examplewebsitename.com.au/Lorem-Ipsum-Dolor-Sit-Amet-Consectetur-Adipiscing-Elit.
+                </td>
+              </tr>{" "}
+              <tr className="border-b border-[#BFBFBF] text-sm">
+                <td className="py-3 font-light">000000</td>
+                <td className="py-3 font-light">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit...
+                </td>
+                <td className="py-3 font-light">
+                  https://www.examplewebsitename.com.au/Lorem-Ipsum-Dolor-Sit-Amet-Consectetur-Adipiscing-Elit.
+                </td>
+              </tr>{" "}
+              <tr className="border-b border-[#BFBFBF] text-sm">
+                <td className="py-3 font-light">000000</td>
+                <td className="py-3 font-light">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit...
+                </td>
+                <td className="py-3 font-light">
+                  https://www.examplewebsitename.com.au/Lorem-Ipsum-Dolor-Sit-Amet-Consectetur-Adipiscing-Elit.
+                </td>
+              </tr>{" "}
+              <tr className="border-b border-[#BFBFBF] text-sm">
+                <td className="py-3 font-light">000000</td>
+                <td className="py-3 font-light">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit...
+                </td>
+                <td className="py-3 font-light">
+                  https://www.examplewebsitename.com.au/Lorem-Ipsum-Dolor-Sit-Amet-Consectetur-Adipiscing-Elit.
+                </td>
+              </tr>
+              <tr className="border-b border-[#BFBFBF] text-sm">
+                <td className="py-3 font-light">000000</td>
+                <td className="py-3 font-light">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit...
+                </td>
+                <td className="py-3 font-light">
+                  https://www.examplewebsitename.com.au/Lorem-Ipsum-Dolor-Sit-Amet-Consectetur-Adipiscing-Elit.
+                </td>
+              </tr>
+              <tr className="border-b border-[#BFBFBF] text-sm">
+                <td className="py-3 font-light">000000</td>
+                <td className="py-3 font-light">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit...
+                </td>
+                <td className="py-3 font-light">
+                  https://www.examplewebsitename.com.au/Lorem-Ipsum-Dolor-Sit-Amet-Consectetur-Adipiscing-Elit.
+                </td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div className="flex justify-between mt-5">
+
+      <div className="w-[155px] h-[48px] border-black border rounded-md flex items-center justify-center text-[#000000]">
+<PiPencil size={25}/> <span className="pl-3">Edit Items</span>
+      </div>
+      <div className="w-[155px] h-[48px] border-black border rounded-md bg-[#000] flex items-center justify-center text-[#fff]">
+Stock Details
+      </div>
       </div>
     </div>
   );
